d602d04236f8f16f34ed2447a423d3932b0dd68b
Maksym <yarrok98@gmail.com>
2020-04-03 17:41:40 +0300

    Add Database connection

---
+++ ToDoList/ConfigurationManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ToDoList
+{
+    public static class ConfigurationManager
+    {
+        public static IConfiguration AppSetting { get; set; }
+
+        static ConfigurationManager()
+        {
+            AppSetting = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+    }
+}
