3f172035e56f9cd90a206d0a89b015a7b27ffce2
Maksym <yarrok98@gmail.com>
2020-04-07 11:52:59 +0300

    Add Sign-In UI

---
+++ ToDoList/Models/SignInModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class SignInModel
+    {
+        [Display(Name = "Enter your email")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(30)]
+        [Required]
+        public string Email { get; set; }
+
+        [Display(Name = "Enter your password")]
+        [DataType(DataType.Password)]
+        [StringLength(30)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
