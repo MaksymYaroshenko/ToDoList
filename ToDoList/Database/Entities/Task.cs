88b7911c79b77079edf9c3939bff0061f6e61dc4
Maksym <yarrok98@gmail.com>
2020-04-21 16:26:30 +0300

    Add "Task" table

---
+++ ToDoList/Database/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ToDoList.Database.Entities
+{
+    public class Task
+    {
+        public int ID { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool IsImportant { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
