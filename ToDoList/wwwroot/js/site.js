cac4534c0e5a7f0779a93271da43d89fc20a2afb
Maksym <yarrok98@gmail.com>
2020-04-21 11:49:30 +0300

    Add mock-up of the main page

--- ToDoList/wwwroot/js/site.js
+++ ToDoList/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function reply_click() {
+    document.getElementById('add-icon').hidden = true;
+    document.getElementById('new-task').style.display = '';
+    event.preventDefault();
+}
