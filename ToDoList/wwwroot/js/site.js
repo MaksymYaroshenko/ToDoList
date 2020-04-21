2c91fed8ba18a71a763b2f2b7738bb6bb1776f1a
Maksym <yarrok98@gmail.com>
2020-04-21 13:03:09 +0300

    Update Registration UI

--- ToDoList/wwwroot/js/site.js
+++ ToDoList/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿function reply_click() {
+﻿function add_new_task_click() {
     document.getElementById('add-icon').hidden = true;
     document.getElementById('new-task').style.display = '';
     event.preventDefault();
